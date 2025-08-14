1ef5be2c3686889adf1ddcd463983db9209f8d7a
Ethan L. Mines <elmines@users.noreply.github.com>
2025-08-14 17:01:22 -0400

    Update scripts to my  liking

--- src/eval.sh
+++ src/eval.sh
@@ -1,33 +1,13 @@
-# bash ./eval.sh > eval.log
+#!/usr/bin/bash
 
 ###################################################################################################################
 config=../config/config-bertweet.txt
-test_data=../data/BiLSTM_target_classification.csv
-model_dir=./trained_models
+model_dir=./trained_models/bertweet_multitask
 
-for seed in {1..3}
+predictions_dir=../target-classification/output/targets_bertweet_bad_regex
+for seed in 0 112 343
 do
-    echo "Start training on seed ${seed}......"
+    test_data=${predictions_dir}/predictions_Bertweet_seed_${seed}.csv
+    echo "Start evaluation on seed ${seed}......"
     python eval.py -s ${seed} -c ${config} -test ${test_data} -mod_dir ${model_dir} -m bertweet
 done
-###################################################################################################################
-config=../config/config-bertweet.txt
-test_data=../data/KPTimes_target_generation.csv
-model_dir=./trained_models
-
-for seed in {1..3}
-do
-    echo "Start training on seed ${seed}......"
-    python eval.py -s ${seed} -c ${config} -test ${test_data} -mod_dir ${model_dir} -m bertweet
-done
-###################################################################################################################
-config=../config/config-bertweet.txt
-test_data=../data/KPTimes_target_generation_zero_shot.csv
-model_dir=./trained_models
-
-for seed in {1..3}
-do
-    echo "Start training on seed ${seed}......"
-    python eval.py -s ${seed} -c ${config} -test ${test_data} -mod_dir ${model_dir} -m bertweet
-done
-###################################################################################################################
--- src/train.sh
+++ src/train.sh
@@ -1,51 +1,18 @@
-# bash ./train.sh > train.log
+#!/bin/bash
 
-####################################################################################################################
 config=../config/config-bertweet.txt
 train_data=../data/train.csv
 dev_data=../data/val.csv
 test_data=../data/test.csv
-model_dir=./trained_models
 
-for seed in {1..3}
+# for seed in 0 112 343
+# do
+#     echo "Start training on seed ${seed}......"
+#     python train.py -s ${seed} -c ${config} -train ${train_data} -dev ${dev_data} -test ${test_data} -mod_dir ./trained_models/bertweet_multitask -m bertweet -mul
+# done
+
+for seed in 0 112 343
 do
     echo "Start training on seed ${seed}......"
-    python train.py -s ${seed} -c ${config} -train ${train_data} -dev ${dev_data} -test ${test_data} -mod_dir ${model_dir} -m bertweet
+    python train.py -s ${seed} -c ${config} -train ${train_data} -dev ${dev_data} -test ${test_data} -mod_dir ./trained_models/bertweet_singletask -m bertweet
 done
-###################################################################################################################
-config=../config/config-bertweet.txt
-train_data=../data/train.csv
-dev_data=../data/val.csv
-test_data=../data/test.csv
-model_dir=./trained_models
-
-for seed in {1..3}
-do
-    echo "Start training on seed ${seed}......"
-    python train.py -s ${seed} -c ${config} -train ${train_data} -dev ${dev_data} -test ${test_data} -mod_dir ${model_dir} -m bertweet -mul
-done
-###################################################################################################################
-config=../config/config-bilstm.txt
-train_data=../data/train.csv
-dev_data=../data/val.csv
-test_data=../data/test.csv
-model_dir=./trained_models
-
-for seed in {1..3}
-do
-    echo "Start training on seed ${seed}......"
-    python train.py -s ${seed} -c ${config} -train ${train_data} -dev ${dev_data} -test ${test_data} -mod_dir ${model_dir} -m bilstm
-done
-# ###################################################################################################################
-config=../config/config-bilstm.txt
-train_data=../data/train.csv
-dev_data=../data/val.csv
-test_data=../data/test.csv
-model_dir=./trained_models
-
-for seed in {1..3}
-do
-    echo "Start training on seed ${seed}......"
-    python train.py -s ${seed} -c ${config} -train ${train_data} -dev ${dev_data} -test ${test_data} -mod_dir ${model_dir} -m bilstm -mul
-done
-###################################################################################################################
