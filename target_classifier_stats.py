7bb9152a120c5bccff4e7d1a21b0c346f91a2190
Ethan L. Mines <elmines@users.noreply.github.com>
2025-08-15 15:15:48 -0400

    Add my own target classification eval script

---
+++ target_classifier_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from sklearn.metrics import precision_recall_fscore_support
+from collections import OrderedDict
+import csv
+import sys
+
+slices = OrderedDict([
+    ('SE', slice(0, 1080)),
+    ('AM', slice(1880, 6989)),
+    ('C19', slice(1080, 1880)),
+    ('PS', slice(6989, 9146)),
+    ('All', slice(None))
+])
+
+writer = csv.DictWriter(sys.stdout, fieldnames=list(slices.keys()))
+writer.writeheader()
+
+result_rows = []
+for in_path in sys.argv[1:]:
+    pred_strs = []
+    label_strs = []
+    targets = set()
+    with open(in_path, 'r') as r:
+        reader = csv.DictReader(r)
+        for row in reader:
+            pred_strs.append(row['Mapped Target'])
+            label_strs.append(row['Target'])
+            targets.add(row['Target'])
+    target2id = {t:i for i,t in enumerate(targets)}
+
+    preds = [target2id[t] for t in pred_strs]
+    labels = [target2id[t] for t in label_strs]
+
+    row = {}
+    for dataset_name, inds in slices.items():
+        d_preds = preds[inds]
+        d_labels = labels[inds]
+        _, _, micro_f1, _ = precision_recall_fscore_support(d_labels, d_preds, average='micro')
+        row[dataset_name] = micro_f1
+    result_rows.append(row)
+    writer.writerow(row)
+
+means = {k:sum(row[k] for row in result_rows)/len(result_rows) for k in slices}
+writer.writerow(means)
